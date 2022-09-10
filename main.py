e768b5a78b1e15b53689fb5de95ad179ae4900bf
jessicaprass <jessikprass@gmail.com>
2022-09-09 21:26:50 -0300

    Primeiro commit para criar o projeto

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-# Módulo
-# Requisitos:
+# Programa principal
+# Requisitos: Coleta dados de nóticias políticas na página G1 e os salva em um arquivo no formato html.
 # Autor: Jessica Prass
-# Versão: 0.0.1
+# Versão: 1.0.0
 # Data: 06/09/2022
 
-# Importando o pacote requests
 import requests
 
 # Lendo a página de política do G1
@@ -14,9 +13,8 @@ pagina.status_code
 
 # Gravando o arquivo com as informações da variável pagina
 arquivo = open('pagina.html', 'wb')
-# w = texto
-# wb = vários dados no mémoria, bytes
-for texto in pagina.iter_content(1048576)
+
+for texto in pagina.iter_content(1048576):
     # quantidade de bits lidos de cada vez
     arquivo.write(texto)
 arquivo.close()
